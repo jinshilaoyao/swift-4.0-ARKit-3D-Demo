c6aa1bb7277fdfdd4802143e63adf597b46c1b9c
yesway <yesway@yeswaydeMacBook-Pro.local>
2017-07-10 09:52:13 +0800

    no message

--- ARKit-Demo-3D/ARKit-Demo-3D/ViewController.swift
+++ ARKit-Demo-3D/ARKit-Demo-3D/ViewController.swift
@@ -15,17 +15,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        button = UIButton(frame: CGRect(x: 10, y: 10, width: 100, height: 44))
-        button.backgroundColor = UIColor.red
-        button.addTarget(self, action: #selector(nextPage), for: .touchUpInside)
-        view.addSubview(button)
     }
     @IBAction func clickonTap(_ sender: UIButton) {
         self.navigationController?.pushViewController(ARSCNViewController(), animated: true)
-    }
-    
-    @objc func nextPage() {
     }
 
     override func didReceiveMemoryWarning() {
